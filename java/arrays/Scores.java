8ee5472b591bbc15754a04d2b84387a9f217d806
Lensley <lensley.stfelix@gmail.com>
2026-09-10 08:09:34 -0400

    Add Java array and collection learning exercises

---
+++ java/arrays/Scores.java
@@ -0,0 +1,11 @@
+class Scores {
+    public static void main(String[] args) {
+        int[] scores = {100, 99, 98, 97, 96};
+
+        System.out.println(scores[0]);
+        System.out.println(scores[1]);
+        System.out.println(scores[2]);
+        System.out.println(scores[3]);
+        System.out.println(scores[4]);
+    }
+}
