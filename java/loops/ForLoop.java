fcff284869309ed612454958065d147cbce1da7c
Lensley <lensley.stfelix@gmail.com>
2026-09-12 20:29:15 -0400

    Add Java methods and loops learning exercises

---
+++ java/loops/ForLoop.java
@@ -0,0 +1,11 @@
+class ForLoop {
+    public static void main(String[] args) {
+        System.out.println("This for loop will count to 5: ");
+
+        for (int count = 1; count <= 5; count++) {
+            System.out.println(count);
+        }
+
+        System.out.println("The loop is done.");
+    }
+}
